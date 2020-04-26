65eb6315aff18d7a6eff35a5faea13eae919707c
Grant Grubbs <xetnus@gmail.com>
2020-04-26 17:11:25 -0400

    Create constants for header/trailer

--- jpegcarver.py
+++ jpegcarver.py
@@ -6,11 +6,13 @@ import shutil
 
 BLOCK = 512
 START = 20  # max length the header could be in the starting block
+JPEG_HEADER = bytes.fromhex("FFD8FF")
+JPEG_TRAILER = bytes.fromhex("FFD9")
 
 def create_dump_dir(output_dir="output"):
     if os.path.isdir(output_dir):
-        usr = input("Output directory already exists, do you want to delete \
-            it? [y/n] ").lower()
+        dir_exists = "Output directory already exists, do you want to delete it? [y/n] "
+        usr = input(dir_exists).lower()
         if usr == 'y':
             shutil.rmtree(output_dir)
         else:
@@ -20,8 +22,6 @@ def create_dump_dir(output_dir="output"):
 
 def main(filename: str):
     create_dump_dir()
-    jpeg_header = bytes.fromhex("FFD8FF")
-    jpeg_trailer = bytes.fromhex("FFD9")
     start = 0
     image_num = 0
     header_found = False
@@ -30,18 +30,18 @@ def main(filename: str):
     with open(filename, "rb") as file:  # open bin file
         for i in range(os.path.getsize(filename)):
             if header_found:  # Getting trailer tag
-                search = file.read(len(jpeg_trailer))  # get tag
-                file.seek((1 - len(jpeg_trailer)), os.SEEK_CUR) # move back
+                search = file.read(len(JPEG_TRAILER))  # get tag
+                file.seek((1 - len(JPEG_TRAILER)), os.SEEK_CUR) # move back
             else:             # Getting Header tag
-                search = file.read(len(jpeg_header))
-                file.seek((1 - len(jpeg_header)), os.SEEK_CUR)
+                search = file.read(len(JPEG_HEADER))
+                file.seek((1 - len(JPEG_HEADER)), os.SEEK_CUR)
 
-            if search == jpeg_header and not header_found and (i%BLOCK) < START:
+            if search == JPEG_HEADER and not header_found and (i%BLOCK) < START:
                 print("Found Header at " + str(i))
                 offset = i              # We found the start header, mark offset
                 header_found = True     # Mark flag, start search for trailer
 
-            elif search == jpeg_trailer and header_found:
+            elif search == JPEG_TRAILER and header_found:
                 # disgusting
                 print("Image found at " + "b'" + str(offset) + "' of length " \
                 + "b'" + str(i - offset) + "'\t image" + str(image_num) +".jpg")
